36ffb7217400bd9987378bca710e4b5cbf0a22c8
connornishijima <connornishijima@gmail.com>
2016-02-29 16:02:12 -0700

    Update example.py

--- example.py
+++ example.py
@@ -4,12 +4,19 @@ import time
 pinMode(7,OUTPUT)
 pinMode(6,INPUT)
 
-print "Setting XIO-P7 HIGH FOR 1 SECOND..."
-digitalWrite(7,HIGH)
-time.sleep(1)
+print "Toggling  XIO-P7 10 times..."
+for i in range(0,10):
+        digitalWrite(7,HIGH)
+        time.sleep(0.1)
+        digitalWrite(7,LOW)
+        time.sleep(0.1)
 
-print "Setting XIO-P7 LOW FOR 1 SECOND..."
-digitalWrite(7,LOW)
-time.sleep(1)
+state = digitalRead(6)
+print "Watching state on XIO-P6: (Connect it to ground to see changes! Ctrl-C to end test.)"
+while True:
+        s = digitalRead(6)
+        if s != state:
+                state = s
+                print "State of XIO-P6 (pullup) changed to: "+str(state)
 
-print "State of XIO-P6 is: "+str(digitalRead(6))+" (pullup)"
+print "END"
